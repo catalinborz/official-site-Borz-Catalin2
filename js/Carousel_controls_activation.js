ad5305e4fddc908f514b5e8331a5ff9c0c7ee12b
catalinborz <bcatalinnn@yahoo.com>
2016-12-30 19:37:52 +0200

    uploaded files

---
+++ js/Carousel_controls_activation.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+// Enable Carousel Controls
+$(".left").click(function(){
+	$("#myCarousel").carousel("prev");
+});
+$(".right").click(function(){
+	$("#myCarousel").carousel("next");
+});
+});
